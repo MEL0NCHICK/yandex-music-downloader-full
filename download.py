072ef7703fdc3b6d66c40dcc47a2c0a0214082b9
MEL0NCHICK <137648675+MEL0NCHICK@users.noreply.github.com>
2025-10-25 18:34:42 +0300

    Add files via upload

---
+++ download.py
@@ -0,0 +1,113 @@
+import typing
+import requests
+import time
+from dataclasses import dataclass
+from enum import Enum, auto
+import base64
+import hmac
+import hashlib
+from yandex_music.utils.sign_request import DEFAULT_SIGN_KEY
+import re
+from hashlib import md5
+
+
+class Container(Enum):
+    FLAC = auto()
+    MP3 = auto()
+    MP4 = auto()
+
+
+class Codec(Enum):
+    FLAC = auto()
+    MP3 = auto()
+    AAC = auto()
+
+
+@dataclass
+class FileFormat:
+    container: Container
+    codec: Codec
+
+@dataclass
+class CustomDownloadInfo:
+    quality: str
+    file_format: FileFormat
+    urls: list[str]
+    decryption_key: str
+    bitrate: int
+
+FILE_FORMAT_MAPPING = {
+    "flac": FileFormat(Container.FLAC, Codec.FLAC),
+    "flac-mp4": FileFormat(Container.MP4, Codec.FLAC),
+    "mp3": FileFormat(Container.MP3, Codec.MP3),
+    "aac": FileFormat(Container.MP4, Codec.AAC),
+    "he-aac": FileFormat(Container.MP4, Codec.AAC),
+    "aac-mp4": FileFormat(Container.MP4, Codec.AAC),
+    "he-aac-mp4": FileFormat(Container.MP4, Codec.AAC),
+}
+
+
+
+
+SIGN_SALT = 'XGRlBW9FXlekgbPrRHuSiA' #Незнаю что это, но в https://github.com/MarshalX/yandex-music-api/ на чьей основе я сделал скрипт оно было, так что пусть будет
+
+def download_file(track_id: int, filename: str, token: str):
+    """  "quality":
+        LOW = "lq"
+        NORMAL = "nq"
+        HIGH = "hq"
+        LOSSLESS = "lossless"
+    """
+
+    params = {
+            "ts": int(time.time()),
+            "quality": "lossless",
+            "codecs": ",".join(FILE_FORMAT_MAPPING.keys()),
+            "transports": "encraw"
+        }
+
+    header = {
+        "Authorization": f"OAuth {token}",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Mobile Safari/537.36",
+        "X-Yandex-Music-Client": "WindowsPhone/3.20"
+    }
+
+    hmac_sign = hmac.new(
+            DEFAULT_SIGN_KEY.encode(),
+            "".join(str(e) for e in params.values()).replace(",", "").encode(),
+            hashlib.sha256,
+        )
+
+    sign = base64.b64encode(hmac_sign.digest()).decode()[:-1]
+
+    params["sign"] = sign
+
+    response = requests.get(
+            f"https://api.music.yandex.net/tracks/{track_id}/download-info", params=params, headers=header
+        )
+    print("\n", response.json())
+
+    textresp = response.json()
+
+    dlink = textresp['result'][0]['downloadInfoUrl']
+    print("\n", "Тут бурем ссылку для скачивания:", dlink)
+
+    downreq = requests.get(url=dlink)
+    print("\n", "Из этого мы создаем ссылку для скачивания:", downreq.text)
+
+
+    host = re.search(r"<host>(.*?)</host>", downreq.text).group(1)
+    path = re.search(r"<path>(.*?)</path>", downreq.text).group(1)
+    ts = re.search(r"<ts>(.*?)</ts>", downreq.text).group(1)
+    region =re.search(r"<region>(.*?)</region>", downreq.text).group(1)
+    s = re.search(r"<s>(.*?)</s>", downreq.text).group(1)
+    sign = md5((SIGN_SALT + path[1::] + s).encode('UTF-8')).hexdigest()
+
+    download_url = f'https://{host}/get-mp3/{sign}/{ts}{path}'
+    print("\n", "Скачиваем отсюда:", download_url)
+
+
+    file = requests.get(url=download_url)
+    print("\n", "И сохраняем в:", filename)
+    with open(f"{filename}", "wb") as f:
+        f.write(file.content)
---
+++ main.py
@@ -0,0 +1,40 @@
+import requests
+from download import download_file
+
+token = "y0__xCU6tyZBhje-AYgtI-e6xTu4ghvMYZaGLWvVdt-jNBssdvh7Q" #Ваш Oauth токен, где его взять: https://github.com/MarshalX/yandex-music-api/discussions/513#discussioncomment-2729781
+
+#print("Название трека: это называется любовь")
+print("Скрипт from MEL0NCHICKС ищет преимущественно именно по названию трека, и скачивает песню полностью)))")
+search_text=input("Введите название трека или исполнителя: ")
+search_count=input("Введите кол-во результатов поиска (сколько будет найдено): ")
+search_url=f"https://api.music.yandex.ru/search/instant/mixed?text={search_text}&type=track&page=0&pageSize={search_count}"
+
+result = requests.get(url=search_url).json()
+
+tracksid = []
+tracksnames = []
+q=1
+print("   Название - Автор ")
+for i in result['result']['results']:
+    trackid = i['track']['id']
+    print("-"*50)
+    print(q, "Найдено: ", i)
+    name=i['track']['title'] + " - " + i['track']['artists'][0]['name']
+    print(q, " ", name, "  |  ID трека: ", trackid, "  |  ID автора: ", i['track']['artists'][0]['id'])
+    tracksnames.append(name)
+    tracksid.append(trackid)
+    q+=1
+
+
+choise_track = int(input("Выберите номер трека который будет скачан: "))-1
+
+actual_track = tracksid[choise_track]
+filename = (tracksnames[choise_track]+".mp3")
+
+invalid_chars = '<>:"/\\|?*'
+
+for char in invalid_chars:
+    filename = filename.replace(char, " ")
+
+print("Скачиваем: ", actual_track, filename)
+download_file(actual_track, filename, token)
